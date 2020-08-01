0d9d01852e17aa1d5831d0d577b0b1c0594d74d5
bkp3 <bholakumar.7372@gmail.com>
2020-08-01 09:35:49 +0530

    updated

--- app/src/main/java/bkp/com/imagefirebasestorage/MainActivity.java
+++ app/src/main/java/bkp/com/imagefirebasestorage/MainActivity.java
@@ -1,14 +1,70 @@
 package bkp.com.imagefirebasestorage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.auth.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("data");
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Member> options = new FirebaseRecyclerOptions.Builder<Member>().setQuery(databaseReference,Member.class).build();
+
+        FirebaseRecyclerAdapter<Member, ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member model) {
+
+                        holder.setDetails(getApplicationContext(), model.getTitle(),model.getImage());
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image,parent, false);
+                        ViewHolder holder = new ViewHolder(view);
+                        return holder;
+                    }
+                };
+        firebaseRecyclerAdapter.startListening();
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
     }
 }
--- app/src/main/java/bkp/com/imagefirebasestorage/ViewHolder.java
+++ app/src/main/java/bkp/com/imagefirebasestorage/ViewHolder.java
@@ -1,7 +1,37 @@
 package bkp.com.imagefirebasestorage;
 
-class ViewHolder {
+import android.content.Context;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 
+class ViewHolder extends RecyclerView.ViewHolder {
 
+    View view;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        view = itemView;
+    }
+
+    public void setDetails(Context context, String title, String image){
+
+        TextView textView = view.findViewById(R.id.rTitleView);
+        ImageView imageView = view.findViewById(R.id.rImageView);
+
+        textView.setText(title);
+        Picasso.get().load(image).into(imageView);
+
+        Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+        itemView.startAnimation(animation);
+    }
 }
